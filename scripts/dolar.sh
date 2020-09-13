c9a4442dde83eb4bea380b54b1cd4f7dd902292f
user.name <erick.xferreira@gmail.com>
2020-09-12 23:04:20 -0300

    fix dolar api

--- scripts/dolar.sh
+++ scripts/dolar.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-#
-#get api response
-#
-DOLAR_HOJE=$(curl -s https://economia.awesomeapi.com.br/json/all/USD-BRL | grep -o "ask[^,]*," | grep -o "[0-9].*[0-9]")
 
-#date
-date
-
-# print in terminal
-echo "Dolar: US$ 1"
-echo "R$ $DOLAR_HOJE"
+IP=$( curl -s ifconfig.me )
+#IP='199.119.74.245' test ip
+IPINFO=$( curl -s ipinfo.io/$IP )
+COUNTRY_CODE=$(echo $IPINFO | grep -o "country[^,]*" |  grep -o "[^:]*$" | grep -o "[a-zA-Z].*[^\"]")
 
 
+CURRENCIE_INFO=$(curl -s https://restcountries.eu/rest/v2/alpha/$COUNTRY_CODE )
+CURRENCIE=$(echo $CURRENCIE_INFO | grep -o "currencies[^,]*" | grep -o "[^:]*$" | grep -o "[a-zA-Z].*[^\"]")
 
+
+SYMBOL=$(echo $CURRENCIE_INFO | grep -o "symbol[^,]*" | grep -o "[^:]*$" | grep -oP ".*[\"^]" | grep -o "[^\"]*")
+
+
+#api 1 alternative
+#VALUE=$(curl -s https://economia.awesomeapi.com.br/json/all/USD-$CURRENCIE | grep -o "ask[^,]*," | grep -o "[0-9].*[0-9]")
+
+#api 2 alternative
+CURRENCIES=$(curl -s https://api.exchangeratesapi.io/latest?base=USD )
+VALUE=$(echo $CURRENCIES | grep -o "$CURRENCIE[^,]*" | grep -o "[^:]*$")
+
+
+#echo $COUNTRY_CODE
+#echo $IPINFO
+#echo $CURRENCIE
+
+echo "$SYMBOL: $VALUE"
 
 
 
