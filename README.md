9a6e78ec79bfb50738ac4b853555cff4f3fb6c88
user.name <erick.xferreira@gmail.com>
2020-10-12 22:13:48 -0300

    fix deploy options

--- README.md
+++ README.md
@@ -13,8 +13,7 @@ Simple scripts bash collection
 
 
 * [up.sh](https://github.com/erickferreir4/bash-scripts/blob/master/scripts/up.sh): simple script to climb to parent folder.
-* [deploy-v2.sh](https://github.com/erickferreir4/bash-scripts/blob/master/scripts/deploy-v2.sh): simple script to deploy modified files to a server witch options.
-* [deploy.sh](https://github.com/erickferreir4/bash-scripts/blob/master/scripts/deploy.sh): simple script to deploy modified files to a server.
+* [deploy.sh](https://github.com/erickferreir4/bash-scripts/blob/master/scripts/deploy.sh): simple script to deploy to a server.
 * [dolar.sh](https://github.com/erickferreir4/bash-scripts/blob/master/scripts/dolar.sh): simple script to see the dollar quote for local currency.
 * [question.sh](https://github.com/erickferreir4/bash-scripts/blob/master/scripts/question.sh): simple question script.
 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,39 +1,207 @@
 #!/bin/bash
 
+####################################
+#
+# ./deploy.sh watch
+#
+# - watch files modification to deploy in server
+#
+####################################
+
+####################################
+#
+# ./deploy.sh all
+#
+# - deploy all files
+#
+####################################
+
+####################################
+#
+# ./deploy.sh config
+#
+# - config ssh and root server
+#
+####################################
+
+####################################
+#
+# ./deploy.sh
+#
+# - deploy files last time
+#
+####################################
+
 
 #
-# time file modified
+# Create config to deploy
 #
-FILES=$(find . -cmin -60)
+create_config()
+{
+	if [ -f ".deploy_config" ]; then
+		chmod ugo=rw .deploy_config
+		DATE_FILE=$(date -r .deploy_config)
+	else
+		echo -e ".git\n.deploy_config\n/node_modules\npackage-lock.json\ndeploy_error.log" > .deployignore
+	fi
 
 
-# scp to copy files
-# ex
-# SCP="name@host:~/Desktop/App"
-SCP="name-host@host-address:~/Desktop/App"
+	read -p "SSH: " SSH
+	echo "SSH='$SSH'" > .deploy_config
+
+	read -p "ROOT: " ROOT
+	echo "ROOT='$ROOT'" >> .deploy_config
+
+	chmod ugo=r .deploy_config
 
 
-# base dir to project main
-# ex
-# BASEDIR="~/Desktop/App"
-BASEDIR="~/dir/app"
+	if [ ! "$DATE_FILE" = '' ]; then
+		touch -d "$DATE_FILE" .deploy_config
+	fi
+
+	exit
+}
 
 #
-# SSH
+# Check has config deploy
 #
-# ssh-keygen required in the server
+check_config () 
+{
+	if [ ! -f ".deploy_config" ]; then
+		create_config
+	fi
+
+	source .deploy_config
+}
+
 #
-SSH="ssh name-host@host-address"
+# Watch files modification
+#
+listening_files()
+{
+	sleep 2 
+
+	FILES_MODIFY=$(find . -newer .deploy_config -type f)
+
+	for file in $FILES_MODIFY; do
+		if [ -f $file ];then
+			deploy_files $file
+		fi
+	done
+
+	listening_files
+}
+
+#
+# Deploy files in server
+#
+deploy_files()
+{
+
+	DIR=$( echo $file | grep -o "[^/].*/" | grep -o "[^.].*" )
+
+	FILES_IGNORE=$(cat .deployignore)
+
+	for ignore in $FILES_IGNORE; do
+		if [[ "$file" =~ "$ignore" ]]; then
+			return 1
+		fi
+	done
 
 
-for file in $FILES; do
-	if [ -f $file ]; then
+	echo 
 
-		DIR=$(echo $file | grep -o "[^/].*/" |  grep  -o "[^.].*")
-		$SSH "[ ! -d $BASEDIR$DIR ] && mkdir -p $BASEDIR$DIR && echo \"$DIR   +++++++++++directory created+++++++++++++>   $BASEDIR\""
+	ssh $SSH "[ ! -d $ROOT$DIR ] && mkdir -p $ROOT$DIR" 2> deploy_error.log
 
-		scp -q $file $SCP$DIR
-		echo "$file   +++++++++++copied+++++++++++++>   $SCP$DIR"
+	echo $ROOT$DIR
+	scp $file "$SSH:$ROOT$DIR" 2> deploy_error.log
+
+
+	touch deploy_error.log
+	ERROR=$(cat deploy_error.log)
+
+	if [ "$ERROR" = '' ]; then
+		touch .deploy_config
+	fi
+}
+
+#
+# Deploy all files
+#
+deploy_all()
+{
+	echo "deployng all files"
+
+	FILES_MODIFY=$(find . -maxdepth 1)
+	FILES_IGNORE=$(cat .deployignore)
+
+	PATTERN=$(echo "$FILES_IGNORE")
+	PATTERN=$(echo $PATTERN | tr ' ' '\|')
+
+	UP_FILES=''
+
+	for file in $FILES_MODIFY; do
+		ignore=$(echo "$file" | egrep -o "$PATTERN")
+		if [ "$ignore" = '' -a "$file" != '.' ]; then
+			UP_FILES+="$file "
+		fi
+	done
+
+	scp -r $UP_FILES "$SSH:$ROOT$DIR" 2> deploy_error.log
+}
+
+#
+# Deploy modified files
+#
+deploy_modified()
+{
+	FILES_MODIFY=$(find . -newer .deploy_config -type f)
+
+	if [ "$FILES_MODIFY" = '' ]; then
+		echo "no files modified for deployment"
+	else
+		echo "deployng files modified"
+	fi
+
+	for file in $FILES_MODIFY; do
+		if [ -f $file ];then
+			deploy_files $file
+		fi
+	done
+}
+
+#
+# Main program
+#
+main()
+{
+	check_config	
+
+	echo
+	echo "ssh: $SSH"
+	echo "root: $ROOT"
+	echo
+
+	if [ "$1" = '' ]; then
+		deploy_modified 
+
+	elif [ "$1" = 'all' ]; then
+		deploy_all
+
+	elif [ "$1" = 'config' ]; then
+		create_config
+
+	elif [ "$1" = 'watch' ]; then
+		echo 'listening...'
+		listening_files
+
+	else
+		echo 'invalid command line'
 
 	fi
-done
+}
+
+
+main $1
+
+
