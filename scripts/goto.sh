12e9852c55878536fc4ea05944b85a2a24641005
user.name <erick.xferreira@gmail.com>
2020-10-24 23:15:28 -0300

    done goto

---
+++ scripts/goto.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+
+#
+#
+#
+#
+#	Go to dir alias
+#
+#	alias goto=". $HOME/goto.sh"
+#
+#
+#
+#
+#
+#	goto alias
+#
+#	goto add /dir alias
+#
+#
+
+
+create_file()
+{
+	if [ ! -f ".goto" ]; then
+		touch "$HOME/.goto"
+	fi
+}
+
+goto_add()
+{
+	echo "$1=$2" >> .goto
+}
+
+go_to()
+{
+	alias=$1
+
+	dir=$(awk -v var="$alias" -F "=" '{if(var == $2) print $1}' $HOME/.goto)
+
+	cd $dir
+}
+
+
+main()
+{
+	create_file
+
+	if [ "$1" == "add" ]; then
+		goto_add $2 $3
+	else
+		go_to $1
+	fi
+}
+
+
+
+main $1 $2 $3
+
+
+
