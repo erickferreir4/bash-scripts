aad5d5afbf01a879e0525af369d0f48614fe6362
user.name <erick.xferreira@gmail.com>
2020-10-31 20:55:08 -0300

    feature multiple repo

--- scripts/create_repos.sh
+++ scripts/create_repos.sh
@@ -5,7 +5,7 @@
 #
 #	Create repository in github
 #
-#	./create_repos.sh name_repo
+#	./create_repos.sh repo1 repo2
 #
 #
 
@@ -16,9 +16,11 @@ GITHUB_PASS=''
 
 main()
 {
-	if [ -n $1 ] && [ "$1" != "" ]; then
-		curl -u "$GITHUB_USER:$GITHUB_PASS" https://api.github.com/user/repos -d '{"name": "'$1'"}'
-	fi
+	for repo in $@; do
+		if [ -n $repo ] && [ "$1" != "" ]; then
+			curl -u "$GITHUB_USER:$GITHUB_PASS" https://api.github.com/user/repos -d '{"name": "'$repo'"}'
+		fi
+	done
 }
 
-main $1
+main $@
