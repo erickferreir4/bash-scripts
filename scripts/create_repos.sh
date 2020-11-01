d4226be3e73424d65b924b4088b11ae3c995dfc2
user.name <erick.xferreira@gmail.com>
2020-10-31 21:10:58 -0300

    change to token

--- scripts/create_repos.sh
+++ scripts/create_repos.sh
@@ -5,20 +5,40 @@
 #
 #	Create repository in github
 #
+#
+#	CONFIG
+#
+#		# Create personal access token #
+#			
+#		1 - github > settings > Developer settings > Personal access tokens			
+#			
+#		2 - click Generate new token	
+#			
+#		3 - name token			
+#			
+#		4 - Select scopes "repo", "admin:repo" 			
+#			
+#		5 - click Generate token			
+#			
+#		6 - copy token			
+#			
+#		7 - paste in GITHUB_TOKEN			
+#			
+#			
+#			
 #	./create_repos.sh repo1 repo2
 #
 #
+#
 
-
-GITHUB_USER=''
-GITHUB_PASS=''
+GITHUB_TOKEN='TOKEN'
 
 
 main()
 {
 	for repo in $@; do
 		if [ -n $repo ] && [ "$1" != "" ]; then
-			curl -u "$GITHUB_USER:$GITHUB_PASS" https://api.github.com/user/repos -d '{"name": "'$repo'"}'
+			curl -H 'Authorization: token '$GITHUB_TOKEN'' https://api.github.com/user/repos -d '{"name": "'$repo'"}'
 		fi
 	done
 }
